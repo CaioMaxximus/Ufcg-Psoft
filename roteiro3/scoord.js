2ae885103725671459a70c4659753724f1fc0fc1
CaioMaxximus <caio.soares@ccc.ufcg.edu.br>
2019-10-20 23:42:06 -0300

    novas entidades e testes

--- roteiro3/scoord.js
+++ roteiro3/scoord.js
@@ -1,4 +1,8 @@
 exports.disciplina = disciplina;
+exports.turma = turma;
+exports.estudante = estudante;
+let assert = require('assert');
+
 
 function disciplina(_id,_nome,_creditos,_pre_requisitos){
     let nome = _nome;  
@@ -13,33 +17,101 @@ function disciplina(_id,_nome,_creditos,_pre_requisitos){
        }
 }
 
-function turma(_id_disciplina, _periodo){
-    let id_disciplina = ref_disciplina;
+function turma(_disciplina, _periodo){
+    let disciplina = _disciplina;
     let periodo = _periodo;
     let status = "planejada";
     let professor = null;
-    let estudates = [];
+    let estudantes = [];
+    let statusValido = function(status_novo){
+        if(status_novo != "planejada" &&  status_novo != "ativa" && status_novo != "concluída"){
+            return false;
+        }
+        return true;
+    };
+    let matricula_novo = function(_aluno){
+        if(!estudantes.includes(_aluno) && (status == "planejada" || status == "ativa") ){
+            estudantes.push(_aluno)
+            return "Matricula Realizada";
+        };
+        return "Matricula Nao Realizada";
+    };
     return{
         set_status : function (novoStatus) {
-            if(novoStatus != "planejada"){
-              return "status invalido"; 
+            if(!statusValido(novoStatus)){
+              return "Status Inválido"; 
             }
             status = novoStatus;
             return "Status Atualizado";
             
         },
         get_professor: () => professor,
-        get_estudantes: () => estudates,
-        get_disciplina : () => id_disciplina,
+        get_estudantes: () => estudantes,
+        get_disciplina : () => disciplina,
         get_status : () => status,
         get_periodo : () => periodo,
-        matricular_aluno : (novoAluno) => "codigo aqui"
+        matricular_aluno : (_aluno) => matricula_novo(_aluno),
+        desmatricular_aluno: (_aluno) => 
+            estudantes = estudantes.filter(function(value){
+                return !(value === _aluno);
+            
+            })
     }    
+}
+
+function professor(_matricula, _nome,_email,_cpf){
+    let matricula = _matricula;
+    let nome = _nome;
+    let email = _email;
+    let cpf = _cpf;
+    let turmas = [];
+    return{
+        get_nome : () => nome,
+        set_nome : (novo_nome) => nome = novo_nome,
+        get_email : () => email ,
+        get_cpf : () => cpf,
+        get_foto : () => url_foto,
+        get_matricula : () => matricula,
+        aloca_turma: (turma) => turmas.push(turma),
+        turmas: function (semestre) {
+                    let saida = [];
+                    turmas.forEach(turma => {
+                        if (turma.get_periodo() == semestre){
+                            saida.push(turma.get_disciplina());
+                        }
+                    });
+                    return saida;
+                },
+         
+
+    }
+}
+
+
+function estudante(_nome,_matricula,_email,_cpf){
+    let matricula = _matricula;
+    let email = _email;
+    let cpf = _cpf;
+    let url_foto = "Sem Foto";
+    return {
+        get_nome : () => nome,
+        set_nome : (novo_nome) => nome = novo_nome,
+        matricula: (turma) => turma.matricular_aluno(self),
+        get_email : () => email ,
+        get_cpf : () => cpf,
+        get_foto : () => url_foto,
+        get_matricula: () => matricula
+    };
 }
 
 
 
 
-
 let d = disciplina("prog","programa1",4,[]);
-console.log(d.get_nome());
+let turm_1 = turma(d ,2);
+let estud_1 = estudante("caio","117210","@gmail.com","123123-1");
+turm_1.matricular_aluno(estud_1);
+let professor1 = professor("123","einstein","@gmail","11-11");
+professor1.aloca_turma(turm_1);
+console.log(d === d);
+
--- roteiro3/test.js
+++ roteiro3/test.js
@@ -1,5 +1,8 @@
 let assert = require('assert');
-let disciplina = require('./scoord').disciplina;
+let scoord = require('./scoord');
+let disciplina = scoord.disciplina;
+let turma = scoord.turma;
+let estudante = scoord.estudante;
 
 describe('factory Disciplina', function() {
   let d0;
@@ -38,4 +41,54 @@ describe('factory Disciplina', function() {
         assert.equal('prog1', d0.id());
     });
 
+});
+
+
+describe('factory turma',function (){
+    let d0;
+    let d1;
+    let d2;
+    let t0;
+    let e0;
+
+    before(async () => {
+        d0 = disciplina('calc1',"calculo1",4,[]);
+        d1 = disciplina("calc2",'calculo2',4,[]);
+        d2 = disciplina("calc3",'calculo3',4,[]);
+        e0 = estudante("caio",117210,"@seila","123");
+        t0 = turma(d0,"2");
+    })
+
+    it('deve criar turmas distintas ',function(){
+        let t1 = turma(d0,"2");
+        let t2 = turma(d1,"3");
+        assert.notEqual(t0,t1);
+        assert.notEqual(t0,t2);
+        assert.notEqual(t1,t2);
+    });
+
+    it('dados de inicializacao corretos',function(){
+        assert.equal(t0.get_disciplina(),d0);
+        assert.equal(t0.get_periodo(),"2");
+        assert.equal(t0.get_status(),'planejada');
+        assert.equal(t0.get_professor(),null);
+        assert.deepEqual(t0.get_estudantes(),[]);
+    });
+
+    it('restricoes na matricula de um estudante e status da disciplina',function(){
+        let t1 = turma(d1,"3");
+        assert.equal(t0.set_status("em_espera"),"Status Inválido");
+        assert.equal(t0.set_status("concluída"),"Status Atualizado");
+        assert.equal(t0.matricular_aluno(e0),"Matricula Nao Realizada");
+        assert.equal(t1.matricular_aluno(e0),"Matricula Realizada");
+        assert.deepEqual(t1.get_estudantes()[0],e0);
+        assert.equal(t1.matricular_aluno(e0),"Matricula Nao Realizada");
+        t1.desmatricular_aluno(e0);
+        assert.deepEqual(t1.get_estudantes(),[]);        
+    })
+});
+
+
+describe('factory professor', function(){
+    
 });
